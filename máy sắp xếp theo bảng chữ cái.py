73e65d3e54c3d72cd0c3588c2a698fa4bc44e456
satoshinnnn <141158586+satoshinnnn@users.noreply.github.com>
2025-01-03 12:36:26 +0700

    Create máy sắp xếp theo bảng chữ cái.py

---
+++ máy sắp xếp theo bảng chữ cái.py
@@ -0,0 +1,5 @@
+references = [ "Yeager, D. S., & Dweck, C. S. (2020). What can be learned from growth mindset controversies? American Psychologist, 75(9), 1269–1284. https://doi.org/10.1037/amp0000794",
+              "Dweck, C. S. (2006). Mindset: The new psychology of success. Random House"]
+references.sort()
+for x in references:
+    print(x)
