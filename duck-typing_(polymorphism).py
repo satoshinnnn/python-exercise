b5fc34897e431483720dea2942f3721712b48e6b
satoshinnnn <141158586+satoshinnnn@users.noreply.github.com>
2025-01-03 12:19:47 +0700

    duck-typing_(polymorphism).py

--- duck-typing_(polymorphism).py
+++ duck-typing_(polymorphism).py
@@ -1,3 +1,4 @@
+# Animal Sound Simulator
 class Dog:
     def make_sound(self):   
         print("woof woof")
@@ -8,12 +9,12 @@ class Cow:
     def make_sound(self):
         print("Moo Moo")
 
-# hàm trigger phương thức make_sound()
+# hàm trigger phương thức make_sound() 
 def play_a_sound(animals):
     for animal in animals:
         animal.make_sound()   # Duck-typing adoption: các con Dog, Cat, Cow có chung method 'make_sound'  
 
-# lập list đối tượng
+# lập list đối tượng các conn vật
 animals = [ Dog(), Cat(), Cow()]
 
 # gọi hàm
